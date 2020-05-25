df724092906884398f5fdcd9836593f756c7a65f
Pete <kneedeepabyss@gmail.com>
2020-05-25 19:26:17 +0300

    auto fire transitions

--- src/components/SimPanel.js
+++ src/components/SimPanel.js
@@ -5,34 +5,65 @@ import {
   fireTransitionOnSequence,
   submitStateToCurrent,
   submitStateToSequence,
+  toggleIsFiring,
 } from "../redux/actions";
 import propTypes from "prop-types";
-import { getInitialState, getSequence } from "../redux/selectors";
+import { getInitialState, getSequence, getIsFiring } from "../redux/selectors";
 
 const mapStateToProps = (state) => ({
   initialState: getInitialState(state),
   sequence: getSequence(state),
+  isFiring: getIsFiring(state),
 });
 
 const mapDispatchToProps = {
   handleFireTransitionOnSequence: fireTransitionOnSequence,
   handleSubmitStateToCurrent: submitStateToCurrent,
   handleSubmitStateToSequence: submitStateToSequence,
+  handleToggleIsFiring: toggleIsFiring,
 };
 
 class SimPanel extends React.Component {
   static propTypes = {
     initialState: propTypes.object,
     sequence: propTypes.object,
+    isFiring: propTypes.bool,
     handleFireTransitionOnSequence: propTypes.func.isRequired,
     handleSubmitStateToCurrent: propTypes.func.isRequired,
     handleSubmitStateToSequence: propTypes.func.isRequired,
+    handleToggleIsFiring: propTypes.func.isRequired,
   };
+
+  fireRef = React.createRef();
+
+  state = {
+    interval: null,
+  };
+
+  autoFire() {
+    const { handleToggleIsFiring } = this.props;
+    const { interval } = this.state;
+
+    handleToggleIsFiring();
+
+    if (interval) {
+      clearInterval(interval);
+      this.setState({
+        interval: null,
+      });
+    } else {
+      const newInterval = setInterval(() => {
+        this.fireRef.current.click();
+      }, 1000);
+      this.setState({ interval: newInterval });
+    }
+  }
 
   render() {
     const {
       initialState,
       sequence,
+      isFiring,
       handleFireTransitionOnSequence,
       handleSubmitStateToCurrent,
       handleSubmitStateToSequence,
@@ -49,17 +80,28 @@ class SimPanel extends React.Component {
           ◄◄
         </ButtonStyled>
         <ButtonStyled
+          ref={this.fireRef}
           onClick={() => {
             handleFireTransitionOnSequence(sequence);
           }}
         >
           {" ►▏"}
         </ButtonStyled>
+        <ButtonStyled
+          onClick={() => {
+            this.autoFire();
+          }}
+        >
+          {isFiring ? " stop" : " auto"}
+        </ButtonStyled>
       </SimPanelStyled>
     );
   }
 }
-
+// handleToggleIsFiring();
+//             setInterval(() => {
+//               if (isFiring === true) this.fireRef.current.click(), 1000;
+//             });
 const SimPanelStyled = styled.div`
   margin-bottom: 5px;
   width: auto;
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,7 @@
-export const getInitialState = state => state.initialState;
-export const getCurrentState = state => state.currentState.graphData;
-export const getStats = state => state.currentState.stats;
-export const getTemporaryState = state => state.temporaryState;
-export const getSequence = state => state.sequence;
-export const getIsEditMenuToggled = state => state.isEditMenuToggled;
+export const getInitialState = (state) => state.initialState;
+export const getCurrentState = (state) => state.currentState.graphData;
+export const getStats = (state) => state.currentState.stats;
+export const getTemporaryState = (state) => state.temporaryState;
+export const getSequence = (state) => state.sequence;
+export const getIsEditMenuToggled = (state) => state.isEditMenuToggled;
+export const getIsFiring = (state) => state.isFiring;
