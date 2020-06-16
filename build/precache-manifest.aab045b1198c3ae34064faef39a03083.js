2ae97ee729b922a5f8df279135270c5b3b405216
Pete <kneedeepabyss@gmail.com>
2020-06-16 18:58:33 +0300

    v2

---
+++ build/precache-manifest.aab045b1198c3ae34064faef39a03083.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bc081947158441947247d495e11e4e",
+    "url": "/project-petri/index.html"
+  },
+  {
+    "revision": "b6061ff46f498844bfd2",
+    "url": "/project-petri/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "4d5232294a1ec7448f4e",
+    "url": "/project-petri/static/js/2.1b9a57b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-petri/static/js/2.1b9a57b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6061ff46f498844bfd2",
+    "url": "/project-petri/static/js/main.d8155258.chunk.js"
+  },
+  {
+    "revision": "da3706c7b71c5ff8fc47",
+    "url": "/project-petri/static/js/runtime-main.f1a8a55c.js"
+  },
+  {
+    "revision": "0830346343a5b83287171113629a26b4",
+    "url": "/project-petri/static/media/logo.08303463.png"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/project-petri/precache-manifest.aab045b1198c3ae34064faef39a03083.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-petri/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
